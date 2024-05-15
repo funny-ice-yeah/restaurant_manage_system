7ca0ae80d311bbe6a550ad38874b1c744af4a1f8
WangYang <2319008062@qq.com>
2024-05-15 15:47:10 +0800

    document: 更新TODO.md

--- src/main/java/main/pojo/User.java
+++ src/main/java/main/pojo/User.java
@@ -1,9 +1,7 @@
 package main.pojo;
 
 import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;

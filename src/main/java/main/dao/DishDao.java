45d571bb4af92d091c31edd4d1384db5ee9bcd18
DiaoZiJie <22307130433@m.fudan.edu.cn>
2024-06-15 20:27:53 +0800

    debug：修复了创建菜品价格不会被计入价格历史的bug feature：将创建菜品记录和创建对应的价格历史整合为事务

--- src/main/java/main/dao/DishDao.java
+++ src/main/java/main/dao/DishDao.java
@@ -2,7 +2,9 @@ package main.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.springframework.stereotype.Repository;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
@@ -15,5 +17,10 @@ public interface DishDao extends BaseMapper<Dish>{
     public List<Dish> selectByRestaurantId(Integer Id);
     public List<Dish> selectMainDishsByRestaurantId(Integer id);
     public String selectLocationByDishId(Integer Id);
+
+    @Options(useGeneratedKeys = true, keyProperty = "dishId")
+    @Insert("INSERT INTO dish (dish_name, category, current_price, description, image_url, is_main_dish, restaurant_id) " +
+            "VALUES (#{dishName}, #{category}, #{currentPrice}, #{description}, #{imageUrl}, #{isMainDish}, #{restaurantId})")
+    public int insert(Dish dish);
 
 }

02b7bbd953702950472ff8126902d0b19f44de7a
DiaoZiJie <22307130433@m.fudan.edu.cn>
2024-06-20 23:35:37 +0800

    sql:建立了索引 document：更新了TODO

--- src/main/resources/sql/restaurant_manage_system.sql
+++ src/main/resources/sql/restaurant_manage_system.sql
@@ -126,6 +126,8 @@ CREATE TABLE IF NOT EXISTS price (
     PRIMARY KEY(dish_id, create_at)
 );
 
+
+
 ALTER TABLE restaurant ADD FOREIGN KEY(canteen_id) REFERENCES canteen(canteen_id) ON DELETE CASCADE;  
 ALTER TABLE dish ADD FOREIGN KEY(restaurant_id) REFERENCES restaurant(restaurant_id) ON DELETE CASCADE;  
 ALTER TABLE `order` ADD FOREIGN KEY(user_id) REFERENCES user(user_id) ON DELETE CASCADE;  
@@ -147,3 +149,59 @@ ALTER TABLE allergy ADD FOREIGN KEY(dish_id) REFERENCES dish(dish_id) ON DELETE 
 ALTER TABLE ingredient ADD FOREIGN KEY(dish_id) REFERENCES dish(dish_id) ON DELETE CASCADE;  
 ALTER TABLE nutrition ADD FOREIGN KEY(dish_id) REFERENCES dish(dish_id) ON DELETE CASCADE;  
 
+-- 主键索引
+CREATE INDEX idx_user_id ON user(user_id);
+CREATE INDEX idx_restaurant_id ON restaurant(restaurant_id);
+CREATE INDEX idx_dish_id ON dish(dish_id);
+CREATE INDEX idx_allergy_dish_id_allergy ON allergy(dish_id, allergy);
+CREATE INDEX idx_ingredient_dish_id_ingredient ON ingredient(dish_id, ingredient);
+CREATE INDEX idx_nutrition_dish_id_nutrition ON nutrition(dish_id, nutrition);
+CREATE INDEX idx_order_id ON `order`(order_id);
+CREATE INDEX idx_order_detail_order_id_dish_id ON order_detail(order_id, dish_id);
+CREATE INDEX idx_message_id ON message(message_id);
+CREATE INDEX idx_dish_review_id ON dish_review(review_id);
+CREATE INDEX idx_restaurant_review_id ON restaurant_review(review_id);
+CREATE INDEX idx_canteen_id ON canteen(canteen_id);
+CREATE INDEX idx_manager_id ON manager(manager_id);
+CREATE INDEX idx_favorite_restaurant_user_id ON favorite_restaurant(user_id);
+CREATE INDEX idx_favorite_restaurant_restaurant_id ON favorite_restaurant(restaurant_id);
+CREATE INDEX idx_favorite_dish_user_id ON favorite_dish(user_id);
+CREATE INDEX idx_favorite_dish_dish_id ON favorite_dish(dish_id);
+CREATE INDEX idx_price_dish_id_create_at ON price(dish_id, create_at);
+
+CREATE INDEX idx_user_role_id ON user(role_id); 
+CREATE INDEX idx_user_gender ON user(gender); -- 用户群体分析
+CREATE INDEX idx_user_age ON user(age);  -- 用户群体分析
+
+CREATE INDEX idx_restaurant_account ON restaurant(account);
+CREATE INDEX idx_restaurant_canteen_id ON restaurant(canteen_id);
+CREATE INDEX idx_restaurant_name ON restaurant(restaurant_name);
+
+CREATE INDEX idx_dish_restaurant_id ON dish(restaurant_id);
+CREATE INDEX idx_dish_name ON dish(dish_name);
+CREATE INDEX idx_dish_is_main_dish ON dish(is_main_dish);--主菜
+
+CREATE INDEX idx_order_user_id ON `order`(user_id);
+CREATE INDEX idx_order_restaurant_id ON `order`(restaurant_id);
+CREATE INDEX idx_order_status ON `order`(order_status);
+CREATE INDEX idx_order_time ON `order`(order_time);--订餐
+
+CREATE INDEX idx_order_detail_order_id ON order_detail(order_id);
+CREATE INDEX idx_order_detail_dish_id ON order_detail(dish_id);
+
+CREATE INDEX idx_message_user_id ON message(user_id);
+CREATE INDEX idx_message_order_id ON message(order_id);
+
+CREATE INDEX idx_dish_review_user_id ON dish_review(user_id);
+CREATE INDEX idx_dish_review_dish_id ON dish_review(dish_id);
+
+CREATE INDEX idx_restaurant_review_user_id ON restaurant_review(user_id);
+CREATE INDEX idx_restaurant_review_restaurant_id ON restaurant_review(restaurant_id);
+
+-- 为 dish_review 表的评分和评论创建索引
+CREATE INDEX idx_dish_review_rating ON dish_review(rating);
+CREATE INDEX idx_dish_review_content ON dish_review(content);
+
+-- 为 restaurant_review 表的评分和评论创建索引
+CREATE INDEX idx_restaurant_review_rating ON restaurant_review(rating);
+CREATE INDEX idx_restaurant_review_content ON restaurant_review(content);
